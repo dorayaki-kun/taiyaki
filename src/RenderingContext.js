658a48a3003130357e724a68f6cf72221e6d1451
Tomohide Takao <dorayaki.tomo@gmail.com>
2015-08-23 14:16:12 +0900

    :white_flower: Conducted a minor modification.

--- src/RenderingContext.js
+++ src/RenderingContext.js
@@ -8,8 +8,8 @@ class RenderingContext {
   }
 
   createProgram( ids ) {
-    let gl      = this.gl;
-    let program = gl.createProgram();
+    const gl      = this.gl;
+    const program = gl.createProgram();
 
     ids.forEach( ( id ) => {
       gl.attachShader( program, this.createShader( id ) );
@@ -29,8 +29,8 @@ class RenderingContext {
   }
 
   createShader( id ) {
-    let gl     = this.gl;
-    let source = document.getElementById( id );
+    const gl     = this.gl;
+    const source = document.getElementById( id );
     let shader;
 
     switch ( source.type ) {
@@ -43,7 +43,6 @@ class RenderingContext {
     default :
       console.error( 'The shader type is not an accepted value.' );
     }
-
     gl.shaderSource( shader, source.text );
     gl.compileShader( shader );
 
@@ -59,12 +58,12 @@ class RenderingContext {
   }
 
   createFrameBuffer( width, height ) {
-    let gl = this.gl;
-    let frameBuffer = gl.createFramebuffer();
+    const gl = this.gl;
+    const frameBuffer = gl.createFramebuffer();
 
     gl.bindFramebuffer( gl.FRAMEBUFFER, frameBuffer);
 
-    let renderBuffer = gl.createRenderbuffer();
+    const renderBuffer = gl.createRenderbuffer();
     gl.bindRenderbuffer( gl.RENDERBUFFER, renderBuffer );
 
     gl.renderbufferStorage( gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, width, height );
@@ -75,7 +74,7 @@ class RenderingContext {
       renderBuffer
     );
 
-    let texture = this.createFrameBufferTexture( width, height );
+    const texture = this.createFrameBufferTexture( width, height );
     gl.bindRenderbuffer( gl.RENDERBUFFER, null );
     gl.bindFramebuffer( gl.FRAMEBUFFER, null );
 
@@ -83,8 +82,8 @@ class RenderingContext {
   }
 
   createFrameBufferTexture( width, height ) {
-    let gl = this.gl;
-    let texture = gl.createTexture();
+    const gl = this.gl;
+    const texture = gl.createTexture();
     gl.bindTexture( gl.TEXTURE_2D, texture );
 
     gl.texImage2D(
@@ -123,16 +122,16 @@ class RenderingContext {
   }
 
   bindVbo( program, vboAttrib ) {
-    let gl       = this.gl;
-    let location = gl.getAttribLocation( program, vboAttrib.name );
+    const gl       = this.gl;
+    const location = gl.getAttribLocation( program, vboAttrib.name );
     gl.bindBuffer( gl.ARRAY_BUFFER, this.createVbo( vboAttrib.value ) );
     gl.enableVertexAttribArray( location );
     gl.vertexAttribPointer( location, vboAttrib.stride, gl.FLOAT, false, 0, 0 );
   }
 
   createVbo( value ) {
-    let gl  = this.gl;
-    let vbo = gl.createBuffer();
+    const gl  = this.gl;
+    const vbo = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vbo );
     gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( value ), gl.STATIC_DRAW );
     gl.bindBuffer( gl.ARRAY_BUFFER, null );
@@ -140,8 +139,8 @@ class RenderingContext {
   }
 
   bindIbo( index ) {
-    let gl  = this.gl;
-    let ibo = gl.createBuffer();
+    const gl  = this.gl;
+    const ibo = gl.createBuffer();
     gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, ibo );
     gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Int16Array( index ), gl.STATIC_DRAW );
     gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, null );
@@ -167,11 +166,13 @@ class RenderingContext {
     gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT );
     gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT );
     
+    gl.bindTexture( gl.TEXTURE_2D, null );
+    
     return texture;
   }
 
   bindTexture( texture, slot ) {
-    let gl = this.gl;
+    const gl = this.gl;
     gl.activeTexture( gl.TEXTURE0 + slot );
     gl.bindTexture( gl.TEXTURE_2D, texture );
   }
@@ -185,7 +186,7 @@ class RenderingContext {
   }
 
   toggleCulFace( enable ) {
-    let gl = this.gl;
+    const gl = this.gl;
     if ( enable ) {
       gl.enable( gl.CULL_FACE );
     } else {
@@ -194,7 +195,7 @@ class RenderingContext {
   }
 
   toggleDepthFunc( enable ) {
-    let gl = this.gl;
+    const gl = this.gl;
     if ( enable ) {
       gl.enable( gl.DEPTH_TEST );
     } else {
@@ -203,7 +204,7 @@ class RenderingContext {
   }
 
   depthFunc() {
-    let gl = this.gl;
+    const gl = this.gl;
     gl.depthFunc( gl.LEQUAL );
   }
   
@@ -239,9 +240,9 @@ class RenderingContext {
   }
 
   bindUniform( program, uniformAttrib ) {
-    let gl = this.gl;
-    let {name, type, value} = uniformAttrib;
-    let location = gl.getUniformLocation( program, name );
+    const gl = this.gl;
+    const { name, type, value } = uniformAttrib;
+    const location = gl.getUniformLocation( program, name );
 
     switch ( type ) {
     case 'matrix4fv':
@@ -273,7 +274,7 @@ class RenderingContext {
   }
 
   clear( color, depth ) {
-    let gl = this.gl;
+    const gl = this.gl;
     let flag = gl.COLOR_BUFFER_BIT;
 
     gl.clearColor( color.r, color.g, color.b, color.a );
@@ -295,14 +296,14 @@ class RenderingContext {
   }
 
   drawArrays( mode, count, first = 0 ) {
-    let gl = this.gl;
+    const gl = this.gl;
     gl.drawArrays( mode, first, count );
   }
 
   drawElements( mode, count, offset = 0 ) {
-    let gl = this.gl;
+    const gl = this.gl;
     gl.drawElements( mode, count, gl.UNSIGNED_SHORT, offset );
   }
 }
 
-export {RenderingContext};
+export { RenderingContext };
